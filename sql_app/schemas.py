9019f283b624e40eb0647e5d9aa54ff41db88499
jsburckhardt <jsburckhardt@gmail.com>
2020-12-16 22:35:22 +1100

    feat(schemas): add pydantic models/schemas

---
+++ sql_app/schemas.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class ItemBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class user(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
